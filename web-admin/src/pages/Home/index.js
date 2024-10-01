67a36bc388023cf7e9c763e1c0a503ba40aafea3
X-ether <x2-fly@outlook.com>
2024-10-01 23:34:36 +0800

    Update index.js

--- web-admin/src/pages/Home/index.js
+++ web-admin/src/pages/Home/index.js
@@ -90,6 +90,10 @@ const Home = () => {
                                                     ? '已启用'
                                                     : '未启用'}
                                             </p>
+                                                  <p>
+                      LINUX DO 身份验证：
+                      {statusState?.status?.linuxdo_oauth === true ? '已启用' : '未启用'}
+                    </p>
                                             <p>
                                                 微信身份验证：
                                                 {statusState?.status?.wechat_login === true
